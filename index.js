dfc4c735b314adfd94546e71677fab40f5cddd88
rashidulislamg71 <jrashidulislam0103@gmail.com>
2026-05-20 09:17:04 +0600

    added mongodb and get api create

--- index.js
+++ index.js
@@ -1,17 +1,56 @@
 const express = require("express");
 const app = express();
+
+const dotenv = require("dotenv");
+dotenv.config();
+
+const { MongoClient, ServerApiVersion } = require("mongodb");
+
+const uri = process.env.MONGODB_URI;
+
 const port = 8000;
 
 const cors = require("cors");
 app.use(cors());
 app.use(express.json());
 
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+
 app.get("/", (req, res) => {
   res.send("Hello Developer!");
 });
 
+async function run() {
+  try {
+    await client.connect();
+
+    const db = client.db("smart-care-plus-data");
+    const doctorsCollection = db.collection("doctors");
+
+    app.get("/appointments", async (req, res) => {
+      try {
+        const result = await doctorsCollection.find().toArray();
+        res.send(result);
+      } catch (error) {
+        console.log(error);
+        res.status(500).send({ message: "Server error" });
+      }
+    });
+
+    console.log("MongoDB connected successfully!");
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+run();
+
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`);
+  console.log(`Server running on port ${port}`);
 });
-
-
